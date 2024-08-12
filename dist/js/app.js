6c1dbc60a86dd64d2123c230795f263cd462161f
gorejkoff <neskazua63@gmail.com>
2024-08-12 19:10:49 +0300

    accound active2

--- dist/js/app.js
+++ dist/js/app.js
@@ -284,12 +284,16 @@ if (INPUTS_CODE.length > 0) {
 }
 
 
+
+const ACCOUND_FORM_ELEMENT = document.querySelector('.accound__form');
 const ACCOUND_FORM = document.forms.personal_data;
+console.log(ACCOUND_FORM);
 function accessForm(boolean) {
    for (let f of ACCOUND_FORM) { f.disabled = boolean };
    if (!boolean) {
       document.querySelector('.accound__data-button').style.display = "block";
       ACCOUND_FORM[0].focus();
+      ACCOUND_FORM_ELEMENT.classList.add('active');
    }
    if (boolean) document.querySelector('.accound__data-button').style.display = "";
 }
