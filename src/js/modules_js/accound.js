0ca6f9add7007c227ad3e32e74b66dfc3f4f7556
gorejkoff <neskazua63@gmail.com>
2024-08-12 18:54:42 +0300

    accound focuse

--- src/js/modules_js/accound.js
+++ src/js/modules_js/accound.js
@@ -1,7 +1,10 @@
 const ACCOUND_FORM = document.forms.personal_data;
 function accessForm(boolean) {
    for (let f of ACCOUND_FORM) { f.disabled = boolean };
-   if (!boolean) document.querySelector('.accound__data-button').style.display = "block";
+   if (!boolean) {
+      document.querySelector('.accound__data-button').style.display = "block";
+      ACCOUND_FORM[0].focus();
+   }
    if (boolean) document.querySelector('.accound__data-button').style.display = "";
 }
 
